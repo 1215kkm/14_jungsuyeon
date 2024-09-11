3cf8239197ae48feb16697ae018d369d71ec2c29
1215kkm <1215kkm@naver.com>
2024-09-11 14:42:03 +0900

    14기 정수연 섹션3 업데이트

--- script/action.js
+++ script/action.js
@@ -2,3 +2,13 @@ $(window).scroll(function(){
     let scrT = $(this).scrollTop()
     $('.num').text(scrT)
 })
+
+
+$('#section2 .arrow').click(function(){
+    $(this).hide();
+    $('.info').fadeIn(500);
+    $(this).siblings('img').attr('src','images/profile_img_o.png').hide().fadeIn(500)
+    $(this).siblings('img').attr('src','images/profile_img_o.png')
+    $(this).parent().siblings('.info_text').fadeIn(500);
+    $('#section2 .content_box').addClass('on')
+})
